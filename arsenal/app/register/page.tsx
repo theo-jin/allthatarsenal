b9961baf243811f8d7b39c21723c3ab47d01f881
Theo Jin <jhb_august@naver.com>
2023-08-18 16:52:25 +0900

    add checking user validation

--- arsenal/app/register/page.tsx
+++ arsenal/app/register/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Input, Button, Grid } from '@nextui-org/react';
 
 export default function Register() {
+  const [showErr, setShowErr] = useState("");
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [validInput, setValidInput] = useState(false);
+
+  useEffect(() => {
+    setShowErr("이메일과 비밀번호를 입력해주세요");
+    // 이메일 및 비밀번호 유효성 검증
+    if (
+      email.includes("@") &&
+      password.length >= 8 &&
+      passwordConfirm.length >= 8
+    )
+      setValidInput(true);
+    else setValidInput(false);
+  }, [email, password, passwordConfirm]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
+    if (password !== passwordConfirm) {
+      setShowErr("비밀번호가 일치하지 않습니다");
+      return;
+    }
     try {
       const response = await fetch('/api/auth/signup', {
         method: 'POST',
@@ -33,7 +51,10 @@ export default function Register() {
     <Grid.Container gap={4} justify="center">
       <form onSubmit={handleSubmit}>
         <Grid>
+        <h2>회원가입</h2>
+      
           <Input
+            size='md'
             clearable
             label="NAME"
             placeholder="Name"
@@ -44,9 +65,10 @@ export default function Register() {
         </Grid>
         <Grid>
           <Input
+           size='md'
             clearable
             label="EMAIL"
-            placeholder="Email"
+            placeholder="Email (@ 포함)"
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -54,14 +76,27 @@ export default function Register() {
         </Grid>
         <Grid>
           <Input.Password
-            labelPlaceholder="Password"
+           size='md'
+            label="Password"
             type="password"
+            placeholder="8자 이상"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Grid>
         <Grid>
-          <Button flat color="primary" type="submit">
+          <Input.Password
+           size='md'
+            label="PasswordConfirm"
+            type="passwordConfirm"
+            placeholder="8자 이상"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+        </Grid>
+        <Grid>
+        <p>{showErr}</p>
+          <Button flat color="primary" type="submit"  disabled={!validInput}>
             Submit
           </Button>
         </Grid>
