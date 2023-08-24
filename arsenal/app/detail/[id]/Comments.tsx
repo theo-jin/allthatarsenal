1f562ec509716cf370ee5a5a51dcf69f969fae52
Theo Jin <jhb_august@naver.com>
2023-08-24 23:24:54 +0900

    Update Comments.tsx

--- arsenal/app/detail/[id]/Comments.tsx
+++ arsenal/app/detail/[id]/Comments.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import { useState ,useEffect } from "react"
-import { Button, Grid,Card, Input,Text} from "@nextui-org/react";
+import { useState, useLayoutEffect } from "react"
+import { Button, Grid, Card, Input, Text } from "@nextui-org/react";
 import Modal from "./Modal"
 import { ObjectId } from "mongodb";
 
 interface CommentItem {
   comment: string;
   _id: ObjectId;
-  author:string;
+  author: string;
   parent: ObjectId;
 }
 
@@ -19,63 +19,64 @@ interface CommentProps {
 }
 
 export default function Comment({ result }: CommentProps) {
-  
-  let [comment,setComment]= useState('')
+
+  let [comment, setComment] = useState('')
   let [data, setData] = useState<CommentItem[]>([]);
 
-  useEffect(()=>{
-    fetch('/api/comment/list?id='+result._id).then(r=>r.json()).then((result)=>{
+  useLayoutEffect(() => {
+    fetch('/api/comment/list?id=' + result._id).then(r => r.json()).then((result) => {
       setData(result)
     })
-  },[data]);
- 
+  }, [data]);
 
 
-  const submitHandler=()=>{
-    fetch('/api/comment/new',{
-     method:'POST',
-     body:JSON.stringify({
-       comment : comment,
-        _id :result._id }) 
-   }).then(()=>fetch('/api/comment/list?id='+result._id).then(r=>r.json()).then((result)=>{
-    setData(result)
-  }))
-}
+
+  const submitHandler = () => {
+    fetch('/api/comment/new', {
+      method: 'POST',
+      body: JSON.stringify({
+        comment: comment,
+        _id: result._id
+      })
+    }).then(() => fetch('/api/comment/list?id=' + result._id).then(r => r.json()).then((result) => {
+      setData(result)
+    }))
+  }
 
   return (
     <Grid.Container justify="center">
-    <Grid xs={12}  justify="center">
-        <Text h2 css={{margin:"0px"}}>
+      <Grid xs={12} justify="center">
+        <Text h2 css={{ margin: "0px" }}>
           Comment
-          </Text>
-        </Grid> 
-     
-        {data.length>0 ? data.map(function(a,i){
-          return(
-            <Grid xs={12}  justify="center">
-               <Card  variant="bordered" borderWeight="normal" 
-               css={{  p: "$4", mw: "500px",borderRadius:"5px" ,margin:"1px",padding:"1px"}} >
-               <Grid.Container justify="center">
-                <Grid xs={7}>#{i+1}. {data[i].comment}  </Grid>
+        </Text>
+      </Grid>
+
+      {data.length > 0 ? data.map(function (a, i) {
+        return (
+          <Grid xs={12} justify="center">
+            <Card variant="bordered" borderWeight="normal"
+              css={{ p: "$4", mw: "500px", borderRadius: "5px", margin: "1px", padding: "1px" }} >
+              <Grid.Container justify="center">
+                <Grid xs={7}>#{i + 1}. {data[i].comment}  </Grid>
                 <Grid xs={4}>작성자:{data[i].author} </Grid>
                 <Grid justify="flex-end" xs={1}>
-                <Modal comment={data[i]}/>
-                </Grid>  
-                </Grid.Container>
-                </Card>
-              
-            </Grid>
-          ) 
-      }): 'Please write the first comment!'}
-     <Grid xs={12}  justify="center">
-        <Input size="lg"  placeholder="Write Comment" 
-        onChange={(e)=>{ setComment(e.target.value) }} />
-        <Button auto css={{ background:"#687076"}}  onPress={submitHandler}>submit</Button>
+                  <Modal comment={data[i]} />
+                </Grid>
+              </Grid.Container>
+            </Card>
 
-        </Grid>
-       
-       
-  
-        </Grid.Container>
+          </Grid>
+        )
+      }) : 'Please write the first comment!'}
+      <Grid xs={12} justify="center">
+        <Input size="lg" placeholder="Write Comment"
+          onChange={(e) => { setComment(e.target.value) }} />
+        <Button auto css={{ background: "#687076" }} onPress={submitHandler}>submit</Button>
+
+      </Grid>
+
+
+
+    </Grid.Container>
   )
 }  
