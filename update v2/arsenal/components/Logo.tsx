2ddae23342b96031c52677f0abf5a5abfeb674cf
Theo Jin <jhb_august@naver.com>
2023-10-15 01:35:45 +0900

    nextui initial setting

---
+++ update v2/arsenal/components/Logo.tsx
@@ -0,0 +1,7 @@
+'use client'
+import { Image } from "@nextui-org/react";
+export const Logo = () => (
+    <Image
+        width={100}
+        src="https://i.pinimg.com/originals/21/5b/24/215b24eee713a7a2796467ff2adae1a5.png" />
+);
