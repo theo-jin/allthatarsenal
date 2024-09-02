d51acd6b26fbbfff961660359618f152de40b4ac
Theo Jin <jhb_august@naver.com>
2024-09-02 11:21:21 +0900

    used Skeleton code on PlayerInfo

--- app/_components/Player/PlayerInfo.tsx
+++ app/_components/Player/PlayerInfo.tsx
@@ -1,24 +1,30 @@
 "use client";
-import React from "react";
-import { Card, CardBody } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import { Card, CardBody, Skeleton } from "@nextui-org/react";
 import { LikeBtn } from "../Buttons/LikeBtn";
 import Image from "next/image";
 
 export default function PlayerInfo({ data }: any) {
+	const [isLoaded, setIsLoaded] =useState(false);
+	useEffect(() => {
+		setIsLoaded(true);
+	}, [data.player.pic2]);
 	return (
 		<div className="flex justify-center">
 			<Card className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]">
 				<CardBody>
 					<div className="grid grid-cols-12 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
 						<div className="relative col-span-12 md:col-span-4">
-							<Image
-								alt="Album cover"
-								className="object-cover rounded-lg"
-								src={data.player.pic2}
-								layout="responsive"
-								width={100}
-								height={200}
-							/>
+							<Skeleton className="rounded-lg" isLoaded={isLoaded}>
+								<Image
+									alt="Album cover"
+									className="object-cover rounded-lg"
+									src={data.player.pic2}
+									layout="responsive"
+									width={100}
+									height={200}
+								/>
+							</Skeleton>
 						</div>
 
 						<div className="flex flex-col col-span-12 md:col-span-8">
